ef40117d72b7cea559b8add31ff414bf055216bb
邓苏宁 <dengsuning@bytedance.com>
2024-06-22 03:03:33 +0800

    chore: package publish

--- lib/main.ts
+++ lib/main.ts
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
-
-import fs from 'fs-extra'
+import pkg from '../package.json'
 import { program } from 'commander'
 import './cmd/incrementCov/entry'
-
-// 读取package.json配置信息
-const pkg = fs.readJsonSync('./package.json')
 
 // 查看版本号
 program.version(pkg.version, '-v, --version')
